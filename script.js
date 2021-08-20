58137cc7c7916e19a002ac882f393f231d220f60
clockln <55965457+clockln@users.noreply.github.com>
2021-08-20 17:32:58 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 window.onload = function(){
 //----------------------------------------------------------------------
+const text_display = document.getElementById('display_area')
 
 const imgs = document.querySelectorAll('img');
 for(let i=0;i<imgs.length;i++){
   imgs[i].addEventListener('click',displayText)
 }
 
-const text_display = document.getElementById('output')
-
 function displayText(){
   const txt = this.getAttribute('alt')
   console.log(txt)
   text_display.value = txt
-  console.log(imgs.length)
 }
-
 
 //----------------------------------------------------------------------
 }
