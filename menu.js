19aa8c1b01f885a03a3bf9494c318c8c95f9de8e
clockln <55965457+clockln@users.noreply.github.com>
2021-09-02 13:35:50 +0900

    Update menu.js

--- menu.js
+++ menu.js
@@ -8,5 +8,5 @@ const textArea = document.getElementById('display_area')
 function menu_open(){
   console.log('click')
   //menu_screen.classList.toggle('menu_open');
-  textArea.value = '未設定'
+  textArea.value = '仮更新：タップで透明度変更、URLにデータ保存'
 }
